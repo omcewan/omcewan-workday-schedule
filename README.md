a57770788c8a8ee0904411393cbc7f99e3cefddd
omcewan <orlandomcewan231@gmail.com>
2022-01-09 16:18:27 -0500

    modify html to display time period of a basic workday

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Work Day Scheduler Starter Code
+
+## Tasks
+* Display Today's Date
+* Containers with normals work day hours
---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+let currentDayEl = document.getElementById("currentDay");
+currentDayEl.innerHTML = moment().format("dddd, MMMM Do, YYYY")
