c50373d2f454c2e10afbb67ca101dd60d20693e5
omcewan <orlandomcewan231@gmail.com>
2022-01-09 22:24:25 -0500

    save button functionality

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Work Day Scheduler Starter Code
 
+## Purpose
+Develop a web based day planner app that will display blocks for standard business hours. The hours will be color coded to indicate if the timme as passed, is present or upcoming. 
+
+## Built With
+* HTML
+* CSS 
+* JavaScript
+
+## Website
+https://omcewan.github.io/omcewan-workday-schedule/
+
+## Screenshots
+
+
 ## Tasks
-* Check if time block is in past present or future and set corresponding classes to display styling
+* Click text box to enter data. 
+* Click save button to save data to local storage so that when refresh browser the save date is present.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 let currentDayEl = document.getElementById("currentDay");
 currentDayEl.innerHTML = moment().format("dddd, MMMM Do, YYYY");
 
+let saveBtn1 = document.getElementById("saveBtn1");
+let saveBtn2 = document.getElementById("saveBtn2");
+let saveBtn3 = document.getElementById("saveBtn3");
+let saveBtn4 = document.getElementById("saveBtn4");
+let saveBtn5 = document.getElementById("saveBtn5");
+let saveBtn6 = document.getElementById("saveBtn6");
+let saveBtn7 = document.getElementById("saveBtn7");
+let saveBtn8 = document.getElementById("saveBtn8");
+let saveBtn9 = document.getElementById("saveBtn9");
+
+let textarea1El = document.getElementById("textarea1");
+let textarea2El = document.getElementById("textarea2");
+let textarea3El = document.getElementById("textarea3");
+let textarea4El = document.getElementById("textarea4");
+let textarea5El = document.getElementById("textarea5");
+let textarea6El = document.getElementById("textarea6");
+let textarea7El = document.getElementById("textarea7");
+let textarea8El = document.getElementById("textarea8");
+let textarea9El = document.getElementById("textarea9");
+
 let row1El = document.getElementById("row1");
 let row2El = document.getElementById("row2");
 let row3El = document.getElementById("row3");
@@ -41,4 +61,57 @@ function checkHour() {
   }
 }
 
-setInterval(checkHour, 1000);
+setInterval(checkHour, 0);
+
+function saveData() {
+    
+}
+
+let saveBtnClicks = {
+  saveBtn1: (saveBtn1.onclick = () => {
+    let row1Value = textarea1El.value;
+    console.log(typeof row1Value);
+  }),
+  saveBtn2: (saveBtn2.onclick = () => {
+    let row2Value = textarea2El.value;
+    console.log(row2Value);
+  }),
+  saveBtn3: (saveBtn3.onclick = () => {
+    let row3Value = textarea3El.value;
+    console.log(row3Value);
+  }),
+  saveBtn4: (saveBtn4.onclick = () => {
+    let row4Value = textarea4El.value;
+    console.log(row4Value);
+  }),
+  saveBtn5: (saveBtn5.onclick = () => {
+    let row5Value = textarea5El.value;
+    console.log(row5Value);
+  }),
+  saveBtn6: (saveBtn6.onclick = () => {
+    let row6Value = textarea6El.value;
+    console.log(row6Value);
+  }),
+  saveBtn7: (saveBtn7.onclick = () => {
+    let row7Value = textarea7El.value;
+    console.log(row7Value);
+  }),
+  saveBtn8: (saveBtn8.onclick = () => {
+    let row8Value = textarea8El.value;
+    console.log(row8Value);
+  }),
+  saveBtn9: (saveBtn9.onclick = () => {
+    let row9Value = textarea9El.value;
+    console.log(row9Value);
+  }),
+};
+
+saveBtnClicks.saveBtn1();
+saveBtnClicks.saveBtn2();
+saveBtnClicks.saveBtn3();
+saveBtnClicks.saveBtn4();
+saveBtnClicks.saveBtn5();
+saveBtnClicks.saveBtn6();
+saveBtnClicks.saveBtn7();
+saveBtnClicks.saveBtn8();
+saveBtnClicks.saveBtn9();
